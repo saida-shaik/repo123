7c17e882268382f67725c9645a2c78cca2c5a4eb
saida shaik <setu.shanu11@gmail.com>
2018-12-05 20:22:54 +0530

    Indentation added

--- src/test/java/baselibrary/BaseClass.java
+++ src/test/java/baselibrary/BaseClass.java
@@ -10,23 +10,23 @@ import org.testng.annotations.BeforeMethod;
 public class BaseClass {
 	public	WebDriver driver;
 	public	ExcelLibrary xlib;
-  @BeforeMethod
-  public void openbrowser() {
-	  xlib=new ExcelLibrary();
-	  driver=new FirefoxDriver();
-	  driver.manage().window().maximize();
-	  driver.get("https://www.jabong.com/");
-	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-String	expectedtitle="Online Shopping Site: Buy Women, Men, Kids Fashion & Lifestyle in India - Jabong";
-String actualtitle = driver.getTitle();
+	@BeforeMethod
+	public void openbrowser() {
+		xlib=new ExcelLibrary();
+		driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.jabong.com/");
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		String	expectedtitle="Online Shopping Site: Buy Women, Men, Kids Fashion & Lifestyle in India - Jabong";
+		String actualtitle = driver.getTitle();
 
-if(actualtitle.equals(expectedtitle)){
-	System.out.println("pass");
-}
-  }
-  @AfterMethod
-  public void closebrowser() {
-	  driver.quit();
-  }
- 
+		if(actualtitle.equals(expectedtitle)){
+			System.out.println("pass");
+		}
+	}
+	@AfterMethod
+	public void closebrowser() {
+		driver.quit();
+	}
+
 }
