965b1d86787ea69859cf3af8f4ac1e00d69a32dd
saida shaik <setu.shanu11@gmail.com>
2018-12-07 18:36:50 +0530

    code changes1

--- src/test/java/projectlibrary/Productcheckout.java
+++ src/test/java/projectlibrary/Productcheckout.java
@@ -48,27 +48,17 @@ public class Productcheckout extends BaseClass{
 		Thread.sleep(3000);
 		driver.findElement(By.xpath("//a[@href='https://www.jabong.com/checkout/']")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//a[@href='https://www.jabong.com/customer/account/create/']")).click();
-		Thread.sleep(2000);
-		String fnm = xlib.getexceldata("Signup",1,0);
-		String lnm = xlib.getexceldata("Signup",1,1);
-		String email =xlib.getexceldata("Signup",1,2);
-		String pwd = xlib.getexceldata("Signup",1,3);
-		String mob = xlib.getexceldata("Signup",1,4);
+		driver.findElement(By.xpath("//a[@href='/customer/account/googlesignin/callback/']")).click();
 		Thread.sleep(3000);
-		driver.findElement(By.id("firstname")).sendKeys(fnm);
-		driver.findElement(By.id("lastname")).sendKeys(lnm);
-		driver.findElement(By.id("email")).sendKeys(email);
-		driver.findElement(By.id("accessKey")).sendKeys(pwd);
-		driver.findElement(By.id("mobile")).sendKeys(mob);
-		driver.findElement(By.id("male")).click();
-		driver.findElement(By.id("btn-signup")).click();
+		driver.findElement(By.id("profileIdentifier")).click();
+
+
+
 		Thread.sleep(3000);
 		String pincode = xlib.getexceldata("address",1,0);
 		String fstnm = xlib.getexceldata("address",1,1);
 		String add1 = xlib.getexceldata("address",1,2);
 		String phone = xlib.getexceldata("address",1,3);
-		
 		driver.findElement(By.id("postcode")).sendKeys(pincode);
 		Thread.sleep(2000);
 		driver.findElement(By.id("first_name")).sendKeys(fstnm);
@@ -79,7 +69,7 @@ public class Productcheckout extends BaseClass{
 		Thread.sleep(2000);
 		driver.findElement(By.id("phone")).sendKeys(phone);
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//button[text()='deliver to this address']")).click();
+		driver.findElement(By.xpath("//button[text()='deliver to this address']")).submit();
 		Thread.sleep(3000);
 
 	}
